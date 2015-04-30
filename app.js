baf88778443249029348dfdb0fbd8697d6947ec9
cmmisura14 <cmmisura14@gmail.com>
2015-04-30 13:42:59 -0500

    Add json file

---
+++ app.js
@@ -0,0 +1,20 @@
+//Ajax
+//REST - standardization of communication with server
+
+//Get - pulls info from server
+//Post - sends info to server
+//Put - modifies existing file in server
+//Delete - deletes file from server
+
+//Connection between client and server
+
+//Ajax - asynchronous javascript and xml
+
+//Request -
+//Response -
+
+//
+
+$(document).ready(function(){
+
+});
